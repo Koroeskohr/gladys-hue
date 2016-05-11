8954920d6c87f6e83e710cc826de20d8217e2169
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-05-11 22:26:22 +0200

    auto configuration of the module, and ability to control hue, brightness, and saturation

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ module.exports = function(sails) {
     var search = require('./lib/search.js');
     var register = require('./lib/register.js');
     var exec = require('./lib/exec.js');
+    var config = require('./lib/config.js');
     
     return {
         search: search,
         register: register,
         exec: exec,
+        config: config,
         initialize: function(next){
             loader.injectControllers( __dirname + '/api/controllers', function(err){
                 next(err);
